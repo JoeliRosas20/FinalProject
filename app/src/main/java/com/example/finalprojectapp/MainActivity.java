e0c9d05fbef7d44af3727b2803d54a0632b7bb99
Joel Rosas <joeli.rosas20@gmail.com>
2021-05-03 18:13:53 -0400

    Changes and fixes were made

--- app/src/main/java/com/example/finalprojectapp/MainActivity.java
+++ app/src/main/java/com/example/finalprojectapp/MainActivity.java
@@ -1,35 +1,80 @@
 package com.example.finalprojectapp;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private LinkedList<String> list = new LinkedList<>();
+    private RecyclerView recyclerView;
+    private ListAdapter listAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("onCreate", "Inside onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                /*
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                */
+                //Add something to list
+                addItemDialog();
             }
         });
+        list.addLast("Shoes");
+        recyclerView = findViewById(R.id.recycler_view);
+        listAdapter = new ListAdapter(this, list);
+        recyclerView.setAdapter(listAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        Log.d("onCreate", "Leaving the onCreate");
+    }
+
+    private void addItemDialog(){
+        final AlertDialog.Builder addDialog = new AlertDialog.Builder(MainActivity.this);
+        addDialog.setTitle("Add a list");
+        final EditText input = new EditText(MainActivity.this);
+        addDialog.setView(input);
+        addDialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String item = input.getText().toString();
+                list.add(item);
+                listAdapter.notifyDataSetChanged();
+            }
+        }).create();
+        addDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        }).create();
+        addDialog.show();
     }
 
     @Override
@@ -45,12 +90,10 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
