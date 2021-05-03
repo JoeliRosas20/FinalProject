458e8f3a91b23241fd15922e242acad1cebc16e1
RobertoMorales12 <54601017+RobertoMorales12@users.noreply.github.com>
2021-05-03 18:04:14 -0400

    Add files via upload

---
+++ app/src/main/java/com/example/finalprojectapp/GotActivity.java
@@ -0,0 +1,47 @@
+package com.example.finalprojectapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class GotActivity extends AppCompatActivity {
+    private FloatingActionButton addFloaty;
+    private Button removeButt;
+    private TextView listNames;
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.got_activity);
+
+        addFloaty = findViewById(R.id.floaty);
+        removeButt = findViewById(R.id.removeButt);
+        listNames = findViewById(R.id.list);
+
+        addFloaty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent tent = new Intent(this, AddActivity.class);
+                startActivity(tent);
+
+
+            }
+        });
+
+
+        removeButt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+            }
+
+        });
+
+    }
+}
